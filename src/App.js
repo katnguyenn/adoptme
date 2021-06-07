575ec5751023fa378c1e0eadff77d028a12e51f1
Katherine Nguyen <nguyenkat01@yahoo.com>
2021-06-07 15:46:34 -0700

    Created SearchParams component to build form and utilized useState hook to enable typing in search form

--- src/App.js
+++ src/App.js
@@ -1,39 +1,11 @@
 import ReactDOM from "react-dom";
-import Pet from "./Pet";
-
-// const App = () => {
-//     return React.createElement(
-//         "div",
-//         // attributes (id, class, aria, etc.)
-//         {},
-//         [
-//             React.createElement("h1", {}, "Adopt Me!"),
-//             React.createElement(Pet, {
-//                 name: "Luna",
-//                 animal: "Dog",
-//                 breed: "Havanese",
-//             }),
-//             React.createElement(Pet, {
-//                 name: "Pepper",
-//                 animal: "Bird",
-//                 breed: "Cockatiel",
-//             }),
-//             React.createElement(Pet, {
-//                 name: "Sudo",
-//                 animal: "Dog",
-//                 breed: "Wheaton Terrier",
-//             }),
-//         ]
-//     );
-// };
+import SearchParams from "./SearchParams";
 
 const App = () => {
     return (
         <div>
             <h1>Adopt Me!</h1>
-            <Pet name="Luna" animal="Dog" breed="Havanese" />
-            <Pet name="Pepper" animal="Bird" breed="Cockatiel" />
-            <Pet name="Beam" animal="Dog" breed="Wheaton Terrier" />
+            <SearchParams />
         </div>
     )
 
