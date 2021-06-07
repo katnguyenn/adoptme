f19fd3e5587206ff32bd86c5bd7b104579c121eb
Katherine Nguyen <nguyenkat01@yahoo.com>
2021-06-07 12:07:14 -0700

    Created Pet component to house pet details

--- src/App.js
+++ src/App.js
@@ -1,10 +1,24 @@
 // Anything capitalized, React assumes you created. Anything lowercase is an actual name of html element.
+
+const Pet = () => {
+    return React.createElement('div', {}, [
+        React.createElement("h2", {}, "Luna"),
+        React.createElement("h3", {}, "Dog"),
+        React.createElement("h3", {}, "Havanese"),
+    ])
+}
+
 const App = () => {
     return React.createElement(
         'div',
         // attributes (id, class, aria, etc.)
         {},
-        React.createElement('h1', {}, 'Adopt Me!')
+        [
+            React.createElement('h1', {}, 'Adopt Me!'),
+            React.createElement(Pet),
+            React.createElement(Pet),
+            React.createElement(Pet),
+        ]
     );
 };
 // Call createElement here to create an instance of the App component
