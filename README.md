5e677310f117d5738217d863938b57c83f5eb95b
Katherine Nguyen <nguyenkat01@yahoo.com>
2021-06-07 13:21:20 -0700

    installed prettier and changed settings in vscode to enable prettier format on save and require config

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # adoptme
+
+Step 1: see what's going on under the hood
+    1. loading react with script tags
+    2. createElement to see how JSX is built
+    
+
+
+
+    Prettier
--- src/App.js
+++ src/App.js
@@ -2,25 +2,40 @@
 
 // props: property passed from one parent component to child component (one way data flow). Makes it easy and flexible to dynamically generate content
 const Pet = (props) => {
-    return React.createElement('div', {}, [
+    return React.createElement("div", {}, [
         React.createElement("h2", {}, props.name),
         React.createElement("h3", {}, props.animal),
         React.createElement("h3", {}, props.breed),
-    ])
-}
+    ]);
+};
+
+
+
 
 const App = () => {
     return React.createElement(
-        'div',
+        "div",
         // attributes (id, class, aria, etc.)
         {},
         [
-            React.createElement('h1', {}, 'Adopt Me!'),
-            React.createElement(Pet, { name: "Luna", animal: "Dog", breed: "Havanese" }),
-            React.createElement(Pet, { name: "Pepper", animal: "Bird", breed: "Cockatiel" }),
-            React.createElement(Pet, { name: "Sudo", animal: "Dog", breed: "Wheaton Terrier" }),
+            React.createElement("h1", {}, "Adopt Me!"),
+            React.createElement(Pet, {
+                name: "Luna",
+                animal: "Dog",
+                breed: "Havanese",
+            }),
+            React.createElement(Pet, {
+                name: "Pepper",
+                animal: "Bird",
+                breed: "Cockatiel",
+            }),
+            React.createElement(Pet, {
+                name: "Sudo",
+                animal: "Dog",
+                breed: "Wheaton Terrier",
+            }),
         ]
     );
 };
 // Call createElement here to create an instance of the App component
-ReactDOM.render(React.createElement(App), document.getElementById('root'))
+ReactDOM.render(React.createElement(App), document.getElementById("root"));
