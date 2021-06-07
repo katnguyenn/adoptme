abcbb0f59fa601398d7d7643aa215305ac3b10b7
Katherine Nguyen <nguyenkat01@yahoo.com>
2021-06-07 16:27:17 -0700

    selector for animal type and breed using useState hook

--- src/SearchParams.js
+++ src/SearchParams.js
@@ -1,20 +1,60 @@
 import { useState } from "react";
 
+const ANIMALS = ["bird", "cat", "dog", "rabbit", "reptile"];
 const SearchParams = () => {
 
     const [location, setLocation] = useState("Seattle, WA");
-    
+    const [animal, setAnimal] = useState("");
+    const [breed, setBreed] = useState("");
+    const breeds=[];
+
     return (
         <div className="search-params">
             <form>
                 <label htmlFor="location">
                     Location
-                    <input id="location" onChange={(e) => setLocation(e.target.value)}value={location} placeholder="Location" />
+                    <input id="location" onChange={(e) => setLocation(e.target.value)} value={location} placeholder="Location" />
                 </label>
-                <button>Submit</button>
-            </form>
-            
-        </div>
+                <label htmlFor="animal">
+                    Animal
+                <select
+                        id="animal"
+                        value={animal}
+                        onChange={e => setAnimal(e.target.value)}
+                        onBlur={e => setAnimal(e.target.value)}
+                    >
+                        {/* the empty space above other animal options */}
+                        <option />
+                        {ANIMALS.map(animal => (
+                                <option value={animal} key={animal}>
+                                    {animal}
+                                </option>
+                            
+                            ))}
+                        </select>
+                        </label>
+                <label htmlFor="breed">
+                   Breed
+                <select
+                        id="breed"
+                        value={breed}
+                        onChange={e => setBreed(e.target.value)}
+                        onBlur={e => setBreed(e.target.value)}
+                    >
+                        {/* the empty space above other animal options */}
+                        <option />
+                        {breeds.map(breed => (
+                            <option value={breed} key={breed}>
+                                {breed}
+                            </option>
+
+                        ))}
+                    </select>
+                </label>
+            <button>Submit</button>
+            </form >
+
+        </div >
     )
 }
 
