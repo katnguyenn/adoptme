ec2a91f08d91d3e1283694058513052d995fe203
Katherine Nguyen <nguyenkat01@yahoo.com>
2021-06-07 12:15:57 -0700

    passed props to Pet component to dynamically input content

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 // Anything capitalized, React assumes you created. Anything lowercase is an actual name of html element.
 
-const Pet = () => {
+// props: property passed from one parent component to child component (one way data flow). Makes it easy and flexible to dynamically generate content
+const Pet = (props) => {
     return React.createElement('div', {}, [
-        React.createElement("h2", {}, "Luna"),
-        React.createElement("h3", {}, "Dog"),
-        React.createElement("h3", {}, "Havanese"),
+        React.createElement("h2", {}, props.name),
+        React.createElement("h3", {}, props.animal),
+        React.createElement("h3", {}, props.breed),
     ])
 }
 
@@ -15,9 +16,9 @@ const App = () => {
         {},
         [
             React.createElement('h1', {}, 'Adopt Me!'),
-            React.createElement(Pet),
-            React.createElement(Pet),
-            React.createElement(Pet),
+            React.createElement(Pet, { name: "Luna", animal: "Dog", breed: "Havanese" }),
+            React.createElement(Pet, { name: "Pepper", animal: "Bird", breed: "Cockatiel" }),
+            React.createElement(Pet, { name: "Sudo", animal: "Dog", breed: "Wheaton Terrier" }),
         ]
     );
 };
