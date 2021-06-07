2bd57738ebd95f8d146b3cbdbf8ff10408f5a78f
Katherine Nguyen <nguyenkat01@yahoo.com>
2021-06-07 14:52:09 -0700

    change components to JSX

--- src/App.js
+++ src/App.js
@@ -1,31 +1,42 @@
-import React from "react";
 import ReactDOM from "react-dom";
 import Pet from "./Pet";
 
+// const App = () => {
+//     return React.createElement(
+//         "div",
+//         // attributes (id, class, aria, etc.)
+//         {},
+//         [
+//             React.createElement("h1", {}, "Adopt Me!"),
+//             React.createElement(Pet, {
+//                 name: "Luna",
+//                 animal: "Dog",
+//                 breed: "Havanese",
+//             }),
+//             React.createElement(Pet, {
+//                 name: "Pepper",
+//                 animal: "Bird",
+//                 breed: "Cockatiel",
+//             }),
+//             React.createElement(Pet, {
+//                 name: "Sudo",
+//                 animal: "Dog",
+//                 breed: "Wheaton Terrier",
+//             }),
+//         ]
+//     );
+// };
+
 const App = () => {
-    return React.createElement(
-        "div",
-        // attributes (id, class, aria, etc.)
-        {},
-        [
-            React.createElement("h1", {}, "Adopt Me!"),
-            React.createElement(Pet, {
-                name: "Luna",
-                animal: "Dog",
-                breed: "Havanese",
-            }),
-            React.createElement(Pet, {
-                name: "Pepper",
-                animal: "Bird",
-                breed: "Cockatiel",
-            }),
-            React.createElement(Pet, {
-                name: "Sudo",
-                animal: "Dog",
-                breed: "Wheaton Terrier",
-            }),
-        ]
-    );
-};
+    return (
+        <div>
+            <h1>Adopt Me!</h1>
+            <Pet name="Luna" animal="Dog" breed="Havanese" />
+            <Pet name="Pepper" animal="Bird" breed="Cockatiel" />
+            <Pet name="Beam" animal="Dog" breed="Wheaton Terrier" />
+        </div>
+    )
+
+}
 // Call createElement here to create an instance of the App component
-ReactDOM.render(React.createElement(App), document.getElementById("root"));
+ReactDOM.render(<App />, document.getElementById("root"));
--- src/Pet.js
+++ src/Pet.js
@@ -2,12 +2,22 @@ import React from "react";
 
 // Anything capitalized, React assumes you created. Anything lowercase is an actual name of html element.
 // props: property passed from one parent component to child component (one way data flow). Makes it easy and flexible to dynamically generate content
+// const Pet = (props) => {
+//     return React.createElement("div", {}, [
+//         React.createElement("h2", {}, props.name),
+//         React.createElement("h3", {}, props.animal),
+//         React.createElement("h3", {}, props.breed),
+//     ]);
+// };
+
 const Pet = (props) => {
-    return React.createElement("div", {}, [
-        React.createElement("h2", {}, props.name),
-        React.createElement("h3", {}, props.animal),
-        React.createElement("h3", {}, props.breed),
-    ]);
+    return (
+        <div>
+            <h2>{props.name}</h2>
+            <h3>{props.animal}</h3>
+            <h3>{props.breed}</h3>
+        </div>
+    );
 };
 
 export default Pet;
