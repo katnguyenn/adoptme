59a26c4247aed3fdf3f90c9b435eb4687a980b4a
Katherine Nguyen <nguyenkat01@yahoo.com>
2021-06-08 13:38:42 -0700

    redirect user back to homepage when an error occurs by using Redirect from react-router-dom

--- src/ErrorBoundary.js
+++ src/ErrorBoundary.js
@@ -1,21 +1,26 @@
 // mostly took this from the React docs
 import { Component } from "react";
-import { Link } from "react-router-dom";
+import { Link, Redirect } from "react-router-dom";
 
 class ErrorBoundary extends Component {
-    state = { hasError: false };
+    state = { hasError: false, redirect: false };
     static getDerivedStateFromError() {
         return { hasError: true }
     }
     componentDidCatch(error, info) {
         // I log this to Sentry, Azure Monitor, New Relic, 
-    console.error("ErrorBoundary caught an error, error, info")
+        console.error("ErrorBoundary caught an error", error, info);
+        // Avoid not redirecting on immediate page load errors
+        setTimeout(() => this.setState({ redirect: true }), 5000)
     }
+
     render() {
-        if(this.state.hasError) {
+        if (this.state.redirect) {
+            return <Redirect to="/" />
+        } else if (this.state.hasError) {
             return (
                 <h2>
-                    This listing has an error. <Link to="/">Click here to go back to the homepage</Link>
+                    This listing has an error. <Link to="/">Click here to go back to the homepage or wait five seconds.</Link>
                 </h2>
             )
         }
