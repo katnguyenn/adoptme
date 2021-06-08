e8fa7e7a46ec2fe514a6e9d032c633cf5ebdeb11
Katherine Nguyen <nguyenkat01@yahoo.com>
2021-06-08 10:58:38 -0700

    setting React into strict mode

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import { StrictMode } from "react";
 import ReactDOM from "react-dom";
 import SearchParams from "./SearchParams";
 
@@ -11,4 +12,8 @@ const App = () => {
 
 }
 // Call createElement here to create an instance of the App component
-ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.render(
+    <StrictMode>
+        <App />
+    </StrictMode>,
+    document.getElementById("root"));
