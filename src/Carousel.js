cd2b5b69c278d49d648bf2da377f6c89c66198f6
Katherine Nguyen <nguyenkat01@yahoo.com>
2021-06-08 13:08:52 -0700

    created event handler to the image carousel so users can change displayed image

--- src/Carousel.js
+++ src/Carousel.js
@@ -10,6 +10,14 @@ class Carousel extends Component {
     // ex: carousel.defaultProps will be available
     static defaultProps = {
         images: ["http://pets-images.dev-apis.com/pets/none.jpg"]
+    };
+
+    // can turn into an arrow function so 'this' keyword is bound to the scope where the function is defined
+    handleIndexClick(event) {
+        this.setState({
+            // comes back as a string from the Dom, so + to make it a number
+            active: +event.target.dataset.index
+        })
     }
 
     render() {
@@ -21,9 +29,13 @@ class Carousel extends Component {
                 <img src={images[active]} alt="animal" />
                 <div className="carousel-smaller">
                     {images.map((photo, index) => (
+                        // eslint-disable-next-line
+                        // img should be a button
                         <img
                             key={photo}
                             src={photo}
+                            data-index={index}
+                            onClick={this.handleIndexClick.bind(this)}
                             className={index === active ? "active" : ""}
                             alt="animal thumbnail"
                         />
