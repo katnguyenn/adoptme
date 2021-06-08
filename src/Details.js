b016e1942718359a0b1b686dfe45c6da3e8fa8d7
Katherine Nguyen <nguyenkat01@yahoo.com>
2021-06-08 12:17:21 -0700

    class component example to render details page for pets

--- src/Details.js
+++ src/Details.js
@@ -1,6 +1,50 @@
-const Details = () => {
-    return <h2>hi</h2>
+import { Component } from "react";
+import { withRouter } from "react-router-dom";
+
+class Details extends Component {
+    constructor() {
+        super();
+
+        this.state = { loading: true };
+    }
+
+    async componentDidMount() {
+        const res = await fetch(
+            // this refers to Details component
+            // prop is passed down from the parent
+            // match and params are coming from react-router in App.js(/details/:id)
+            // match: matched path
+            // params: parameters from the user
+            // id: in /details/:id
+            `http://pets-v2.dev-apis.com/pets?id=${this.props.match.params.id}`
+        )
+        const json = await res.json()
+        this.setState(
+            Object.assign(
+                {
+                    loading: false,
+                },
+                // name, location, description will all be passed in
+                json.pets[0]
+
+            )
+        )
+    }
+    render() {
+        const { animal, breed, city, state, description, name } = this.state;
+        return (
+            <div className="details">
+                <div>
+                    <h1>{name}</h1>
+                    <h2>{animal} - {breed} - {city}, {state}</h2>
+                    <button>Adopt {name}</button>
+                    <p>{description}</p>
+                </div>
+            </div>
+        )
+    }
 }
 
-
-export default Details;
+// by default, react router doesn't pass in all its information to Details
+// so wrapping withRouter to Details is going to pass those props in (higher order component)
+export default withRouter(Details);
