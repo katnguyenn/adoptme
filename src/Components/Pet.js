e43dbe2cdc9a0a8ffe5d92bb2a5e96178d571342
Katherine Nguyen <nguyenkat01@yahoo.com>
2021-06-10 15:23:51 -0700

    added positioning to the image's description containers

--- src/Components/Pet.js
+++ src/Components/Pet.js
@@ -18,11 +18,11 @@ if(images.length) {
 }
 
     return (
-        <Link to={`/details/${id}`} className="pet">
-           <div className="image-container">
+        <Link to={`/details/${id}`} className="relative block">
+           <div>
                <img src={hero} alt={name} />
            </div>
-           <div className="info">
+           <div className="absolute bottom-0 left-0 right-0 bg-gradient-to-tr from-white to-transparent pr-2 pt-2 text-center">
                <h1>{name}</h1>
                <h2>{`${animal} - ${breed} - ${location}`}</h2>
            </div>
