8598ec12a2ce073a2857d2c0240437361a3b0048
Katherine Nguyen <nguyenkat01@yahoo.com>
2021-06-10 15:45:16 -0700

    code split with suspense and React.lazy

--- src/App.js
+++ src/App.js
@@ -1,39 +1,43 @@
-import { StrictMode , useState } from "react";
+import { StrictMode, useState, lazy, Suspense } from "react";
 import ReactDOM from "react-dom";
 import { BrowserRouter as Router, Route, Switch, Link } from "react-router-dom";
-import SearchParams from "./Components/SearchParams";
-import Details from "./Components/Details";
 import ThemeContext from "./Components/ThemeContext";
+
+const Details = lazy(() => import("./Components/Details"));
+const SearchParams = lazy(() => import("./Components/SearchParams"))
 
 const App = () => {
     const theme = useState("darkblue");
     return (
         // anything inside of ThemeContext will be available to components
         <ThemeContext.Provider value={theme}>
-        <div className="p-0 m-0"
-            style={{
-                background: "url(http://pets-images.dev-apis.com/pets/wallpaperA.jpg)"
-            }}
+            <div className="p-0 m-0"
+                style={{
+                    background: "url(http://pets-images.dev-apis.com/pets/wallpaperA.jpg)"
+                }}
             >
-            <Router>
-                <header
-                    className="w-full mb-10 text-center p-7 bg-gradient-to-b from-purple-400 via-pink-500 to-red-500 "
-                >
-                    <Link to="/" className="text-6xl text-white hover:text-gray-200 ">
-                        <h1>Adopt Me!</h1>
-                    </Link>
-                </header>
-                <Switch>
-                    <Route path="/details/:id">
-                        <Details />
-                    </Route>
-                    <Route path="/">
-                        <SearchParams />
-                    </Route>
-                </Switch>
-            </Router>
+                <Suspense fallback={<h2>Loading Route...</h2>}>
+                    <Router>
+                        <header
+                            className="w-full mb-10 text-center p-7 bg-gradient-to-b from-purple-400 via-pink-500 to-red-500 "
+                        >
+                            <Link to="/" className="text-6xl text-white hover:text-gray-200 ">
+                                <h1>Adopt Me!</h1>
+                            </Link>
+                        </header>
+                        <Switch>
+                            <Route path="/details/:id">
+                                <Details />
+                            </Route>
+                            <Route path="/">
+                                <SearchParams />
+                            </Route>
+                        </Switch>
+                    </Router>
+                </Suspense>
 
-        </div>
+
+            </div>
         </ThemeContext.Provider>
     )
 
