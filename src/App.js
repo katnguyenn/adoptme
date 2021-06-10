daeae2adff04325fff58970705981a2a6d4235ba
Katherine Nguyen <nguyenkat01@yahoo.com>
2021-06-10 16:11:44 -0700

    reconfigured app to run on Node.js server using hydrate and removed all reference to window

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import { StrictMode, useState, lazy, Suspense } from "react";
-import ReactDOM from "react-dom";
-import { BrowserRouter as Router, Route, Switch, Link } from "react-router-dom";
+import { Route, Switch, Link } from "react-router-dom";
 import ThemeContext from "./Components/ThemeContext";
 
 const Details = lazy(() => import("./Components/Details"));
@@ -9,6 +8,7 @@ const SearchParams = lazy(() => import("./Components/SearchParams"))
 const App = () => {
     const theme = useState("darkblue");
     return (
+        <StrictMode>
         // anything inside of ThemeContext will be available to components
         <ThemeContext.Provider value={theme}>
             <div className="p-0 m-0"
@@ -17,7 +17,7 @@ const App = () => {
                 }}
             >
                 <Suspense fallback={<h2>Loading Route...</h2>}>
-                    <Router>
+                    
                         <header
                             className="w-full mb-10 text-center p-7 bg-gradient-to-b from-purple-400 via-pink-500 to-red-500 "
                         >
@@ -33,18 +33,15 @@ const App = () => {
                                 <SearchParams />
                             </Route>
                         </Switch>
-                    </Router>
+                    
                 </Suspense>
 
 
             </div>
         </ThemeContext.Provider>
+        </StrictMode>
     )
 
 }
-// Call createElement here to create an instance of the App component
-ReactDOM.render(
-    <StrictMode>
-        <App />
-    </StrictMode>,
-    document.getElementById("root"));
+
+export default App;
